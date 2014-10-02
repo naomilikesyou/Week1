7d902f0a91c3b2d4dc93352a303e3f926d9390fd
naomilikesyou <ntewodros09@gmail.com>
2014-10-01 20:35:06 -0400

    jQuery

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+   alert('Welcome to StarTrackr! Now no longer under police investigation!');
+   console.log("OMG JAVASCRIPT")
+   alert($("#celebs tr").length + "elements!")
+});
